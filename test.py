09cc2d51983526e7ba41028a2e8f0e5dd5cdcc44
wabjanith-byte <wabjanith@gmail.com>
2026-03-14 12:34:45 +0530

    frist_upload

---
+++ test.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+species = ('Adelie', 'Chinstrap', 'Gentoo')
+sex_counts = {
+    'Male': np.array([73, 34, 61]),
+    'Female': np.array([73, 34, 58]),
+}
+width = 0.6  # the width of the bars: can also be len(x) sequence
+
+
+fig, ax = plt.subplots()
+bottom = np.zeros(3)
+
+for sex, sex_count in sex_counts.items():
+    p = ax.bar(species, sex_count, width, label=sex, bottom=bottom)
+    bottom += sex_count
+
+    ax.bar_label(p, label_type='center')
+
+ax.set_title('Number of penguins by sex')
+ax.legend()
+
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Generate some random data
+data = np.random.normal(100, 15, 200)
+
+# Create the box plot
+plt.boxplot(data)
+
+# Add a title and show the plot
+plt.title('Basic Box Plot with Matplotlib')
+plt.show()
+
+
+#-----------------------------------
